44ab5d73861b281c055db9bbfc82bb87cfde8d1a
Celeromero0808 <115805225+Celeromero0808@users.noreply.github.com>
2024-03-21 13:51:36 -0300

    ahora si :alien:

--- 01_celeste.py
+++ 01_celeste.py
@@ -1,58 +1,59 @@
 import random 
-class Personajes:
-    vida=100
+class Personaje:
 
     def __init__(self,vida,ataque,nombre) :
         self.vida= vida
         self.ataque=ataque
         self.nombre=nombre
 
-        def esta_vivo (self):
-            return esta_vivo > 0
-        def daño (self,daño):
-            self.vida -= daño
-            if self.vida <0 :
-                self.vida =0
-        def atacar_enemigo(self , enemigo):
-            daño=random.randint(0,self.ataque)
-            enemigo.recibir_daño(daño)
-            print(f"{self.nombre} ataca a {enemigo.nombre} le inflinge{daño}")
+    def esta_vivo(self):
+        return self.vida > 0
 
-            if not enemigo.esta_vivo():
-                print(f"{enemigo.nombre} fue vencido")
+    def recibir_daño (self,daño):
+        self.vida -= daño
+        if self.vida < 0:
+            self.vida = 0
+            
+    def atacar_enemigo(self, enemigo):
+        daño=random.randint(0,self.ataque)
+        enemigo.recibir_daño(daño)
+        print(f"{self.nombre} ataca a {enemigo.nombre} le inflinge{ daño} ")
 
-
-    def main ():
-        Red=Personajes ("Red",100,20)
-        Blue=Personajes ("Blue",100,20)
-
-        while Red.esta_vivo and Blue.esta_vivo ():
-
-           Red.atacar_enemigo(Blue) 
-        
-        if Blue.esta_vivo :
-            Blue.atacar_enemigo (Red)
-
-        if Red.esta_vivo():
-
-            print(f"{Red.nombre} GANA¡")
-        else :
-            print(f"{Blue.nombre} GANA ¡")
-
-              
+        if not enemigo.esta_vivo():
+            print(f"{enemigo.nombre} fue vencido")
 
 
+def main():
+    Red=Personaje (100,20,"Red")
+    Blue=Personaje (100,20,"Blue")
+
+    while Red.esta_vivo() and Blue.esta_vivo():
+
+        Red.atacar_enemigo(Blue) 
+    
+        if Blue.esta_vivo() :
+            Blue.atacar_enemigo(Red)
+
+    if Red.esta_vivo():
+
+        print(f"{Red.nombre} GANA¡")
+    else :
+        print(f"{Blue.nombre} GANA ¡")
+
+            
+main()
 
 
 
 
-        
 
     
 
 
-        
-        
 
 
-        
+    
+    
+
+
+    
